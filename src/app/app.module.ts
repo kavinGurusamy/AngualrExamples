7f56e7689200606df9f73730572cf0838be115e2
kavinGurusamy <kavingurusamy07@gmail.com>
2021-03-10 12:18:21 +0530

    First commit Message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from  '@angular/material/button';
+import { TodoComponent } from './todo/todo.component';
+import {TodoModule} from './todo/todo/todo.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    TodoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
